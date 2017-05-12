46f076ef944220efa2442bde29fc56fe44b5226a
djzaamir <djzaamir@gmail.com>
2017-05-12 20:12:52 +0500

    initial commit

---
+++ Assets/Database.sql
@@ -0,0 +1,46 @@
+
+create database clinic;
+
+create table Doctor (
+   doctor_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT ,
+   doctor_specialization_id int(11) NOT NULL ,
+   doctor_name varchar(255)  NOT NULL
+);
+
+
+create table Disease (
+  disease_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT ,
+  disease_name varchar(255) NOT NULL ,
+  disease_description varchar(1000) NOT NULL
+);
+
+
+
+ create table Patient ( 
+ patient_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT ,
+ patient_name varchar(255) NOT NULL ,
+ patient_father_name varchar(255) NOT NULL , 
+ sex boolean NOT NULL , 
+ dob date, 
+ doctor_id int(11) NOT NULL ,
+ FOREIGN KEY (doctor_id) REFERENCES Doctor(doctor_Id) 
+ );
+
+
+
+create table Disease_History(
+   history_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT ,
+   patient_id int(11) NOT NULL ,
+   disease_id int(11) NOT NULL ,
+   FOREIGN KEY (patient_id) REFERENCES Patient(patient_id),
+   FOREIGN KEY (disease_id) REFERENCES Disease(disease_id),
+   history_timestamp timestamp NOT NULL
+ );
+ 
+ create table Prescription_History(
+   prescription_id int(11) not null primary key auto_increment ,
+   patient_id int(11) not null ,
+   prescription varchar(500) not null ,
+   prescription_timestamp timestamp not null,
+   FOREIGN KEY (patient_id) REFERENCES Patient(patient_id)
+ );
---
+++ src/DBA/DBA.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DBA;
+
+/**
+ *
+ * @author djzaamir
+ */
+public class DBA {
+    
+    public static void Sayhello(){
+        System.out.println("Hey now brown cow");
+    }
+    
+}
